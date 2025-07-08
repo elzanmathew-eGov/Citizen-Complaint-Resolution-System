510910fe8f756682457c7a9151cbc69101074c1d
Hari-egov <hariprasad@egovernments.org>
2025-07-08 17:44:51 +0530

    PGR Citizen Navigation Issue

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -62,7 +62,7 @@ function App() {
         enabledModules={enabledModules}
         moduleReducers={moduleReducers}
         defaultLanding="employee"
-        allowedUserTypes={["employee"]}
+        allowedUserTypes={["employee","citizen"]}
       />
     </Suspense>
   );
