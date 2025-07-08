d21c466328631862d87f73268f99845988d2210c
Hari-egov <hariprasad@egovernments.org>
2025-07-08 17:54:58 +0530

    ROLE REMOVED

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRCard.js
@@ -29,9 +29,9 @@ const PGRCard = () => {
     };
   };
 
-  if (!Digit.Utils.didEmployeeHasAtleastOneRole(Object.values(ROLES).flatMap((e) => e))) {
-    return null;
-  }
+  // if (!Digit.Utils.didEmployeeHasAtleastOneRole(Object.values(ROLES).flatMap((e) => e))) {
+  //   return null;
+  // }
 
   let links = [
     generateLink("ACTION_TEST_CREATE_COMPLAINT", "create-complaint"),
