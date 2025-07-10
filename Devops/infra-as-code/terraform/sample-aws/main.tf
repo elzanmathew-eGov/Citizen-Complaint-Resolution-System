7888109f8c2ab2c553728c1095cdc4e2945951f5
Shivam Upadhyay <145538335+shivam-eGov@users.noreply.github.com>
2025-07-10 12:38:46 +0530

    block public access

--- Devops/infra-as-code/terraform/sample-aws/main.tf
+++ Devops/infra-as-code/terraform/sample-aws/main.tf
@@ -67,10 +67,10 @@ resource "aws_s3_bucket" "filestore_bucket" {
 resource "aws_s3_bucket_public_access_block" "filestore_bucket_access" {
   bucket = aws_s3_bucket.filestore_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "filestore_bucket_policy" {
