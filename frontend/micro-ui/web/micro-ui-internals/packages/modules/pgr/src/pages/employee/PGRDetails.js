d986918b06bb8355f9103b8b2a1093ce03e36506
Hari-egov <hariprasad@egovernments.org>
2025-08-11 19:00:59 +0530

    CCSD-627,CCSD-629

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/pages/employee/PGRDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/pages/employee/PGRDetails.js
@@ -266,7 +266,7 @@ const PGRDetails = () => {
       service: { ...pgrData?.ServiceWrappers[0].service },
       workflow: {
         action: selectedAction.action,
-        assignes: [_data?.SelectedAssignee?.uuid],
+        assignes: _data?.SelectedAssignee?.uuid ? [_data?.SelectedAssignee?.uuid] : null,
         hrmsAssignes: _data?.SelectedAssignee?.uuid ? [_data?.SelectedAssignee?.uuid] : null,
         comments: _data?.SelectedComments || "",
       },
